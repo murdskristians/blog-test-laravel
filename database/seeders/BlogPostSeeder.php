945c00927c75ac4227c95ad2391b22e55798ba4b
Kristians <murdskristians@gmail.com>
2024-07-05 15:48:13 +0300

    updated seeds

--- database/seeders/BlogPostSeeder.php
+++ database/seeders/BlogPostSeeder.php
@@ -5,12 +5,22 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\BlogPost;
 use App\Models\Category;
+use App\Models\User;
 
 class BlogPostSeeder extends Seeder
 {
     public function run()
     {
-        BlogPost::factory()->count(5)->create()->each(function ($post) {
+        // Create 2 blog posts with user_id = 1
+        BlogPost::factory()->count(2)->create([
+            'user_id' => 1,
+        ])->each(function ($post) {
+            $categories = Category::inRandomOrder()->take(rand(0, 2))->pluck('id');
+            $post->categories()->attach($categories);
+        });
+
+        // Create 3 more blog posts with random user_id
+        BlogPost::factory()->count(3)->create()->each(function ($post) {
             $categories = Category::inRandomOrder()->take(rand(0, 2))->pluck('id');
             $post->categories()->attach($categories);
         });
--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -5,16 +5,23 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Comment;
 use App\Models\BlogPost;
-use App\Models\User;
 
 class CommentSeeder extends Seeder
 {
     public function run()
     {
+        // Create 5 comments with user_id = 1
+        BlogPost::all()->random(5)->each(function ($post) {
+            Comment::factory()->create([
+                'blog_post_id' => $post->id,
+                'user_id' => 1,
+            ]);
+        });
+
+        // Create 3 comments for each blog post with random user_id
         BlogPost::all()->each(function ($post) {
             Comment::factory()->count(3)->create([
                 'blog_post_id' => $post->id,
-                'user_id' => User::inRandomOrder()->first()->id,
             ]);
         });
     }
