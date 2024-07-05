5e1413e2f8d32db8d1676f24c6771b593bcffef6
Kristians <murdskristians@gmail.com>
2024-07-06 02:00:34 +0300

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/blog_posts', [BlogPostController::class, 'store']);
     Route::get('/blog_posts/{id}', [BlogPostController::class, 'show']);
     Route::put('/blog_posts/{id}', [BlogPostController::class, 'update']);
-    Route::delete('/blog_posts/{id}', [BlogPostController::class, 'destroy']);
-    Route::post('/blog_posts/{id}/comments', [BlogPostController::class, 'addComment']);
-    Route::put('/comments/{id}', [BlogPostController::class, 'updateComment']);
-    Route::delete('/comments/{id}', [BlogPostController::class, 'deleteComment']);
     Route::get('/categories', [CategoryController::class, 'index']);
 });
 
